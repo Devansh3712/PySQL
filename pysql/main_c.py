51ac62e1303b895d3590fdb886539fe129fc64ee
Devansh3712 <devanshamity@gmail.com>
2021-09-30 22:53:31 +0530

    update

--- pysql/main_c.py
+++ pysql/main_c.py
@@ -81,7 +81,6 @@ while (True):
         user_input = input("pysql> ")
 
     try:
-
         if user_input.lower() in ["-a", "about"]:
             print(info.about)
 
@@ -106,7 +105,6 @@ while (True):
             gh_version = gh_version.read().decode("utf-8")
 
             if gh_version > __version__:
-
                 result = update.update_pysql()
 
                 if result is True:
